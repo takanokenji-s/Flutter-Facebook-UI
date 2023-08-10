a44c69deed6169e010d42a3d3df57706d2a645e3
takanokenji-s <takanokenjisms@gmail.com>
2023-08-11 01:54:02 +0530

    ui fixes

--- lib/screens/welcome.dart
+++ lib/screens/welcome.dart
@@ -1,5 +1,9 @@
+import 'dart:async';
+
+import 'package:facebook_app_ui/screens/profile.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:page_transition/page_transition.dart';
 
 class Welcome extends StatefulWidget {
   const Welcome({super.key});
@@ -9,10 +13,28 @@ class Welcome extends StatefulWidget {
 }
 
 class _WelcomeState extends State<Welcome> {
+  startTime() async {
+    var duration = const Duration(seconds: 2);
+    return Timer(duration, navigationPage);
+  }
+
+  Future<void> navigationPage() async {
+    Navigator.of(context).pushAndRemoveUntil(
+        PageTransition(type: PageTransitionType.fade, child: const Profile()),
+        (route) => false);
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    startTime();
+  }
+
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
     return Scaffold(
+      backgroundColor: const Color(0xff242525),
       body: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
@@ -34,11 +56,10 @@ class _WelcomeState extends State<Welcome> {
               Text(
                 "FACEBOOK",
                 style: GoogleFonts.inter(
-                  color: Colors.black54,
-                  fontWeight: FontWeight.w500,
-                  letterSpacing: 4,
-                  fontSize: 14
-                ),
+                    color: Colors.white60,
+                    fontWeight: FontWeight.w500,
+                    letterSpacing: 4,
+                    fontSize: 14),
               )
             ],
           )
